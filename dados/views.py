6ee783a4aa818f92816bbe0610bf3bbb9b6f8f06
Joel Daniel Perez Camey <dperez@yetis.io>
2020-11-07 07:42:15 -0600

    remove try catch

--- dados/views.py
+++ dados/views.py
@@ -17,25 +17,23 @@ class DadoView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, quantity):
-        if os.environ['REVISAR_JWT'] =='True':
-            try:
-                authorizationHeader = request.META.get('HTTP_AUTHORIZATION')
-                token = authorizationHeader.split()            
-                f = open(os.environ['PUBLIC_JWT'], "r")
-                public_key = f.read()
-                jwt.unregister_algorithm('RS256')
-                jwt.register_algorithm('RS256', RSAAlgorithm(RSAAlgorithm.SHA256))
-                data = jwt.decode(token[1], public_key, verify_aud=False, algorithm='RS256')
-                valid = False                
-                for scope in data['scopes']:
-                    if scope == "dados.tirar":
-                        valid = True
-                print("Scopes de token "+str(data['scopes']))
-                print("El scope buscado es dados.tirar, el token es "+str(valid))
-                if not valid:
-                    return Response(status=status.HTTP_401_UNAUTHORIZED)
-            except:
+        if os.environ['REVISAR_JWT'] =='True':            
+            authorizationHeader = request.META.get('HTTP_AUTHORIZATION')
+            token = authorizationHeader.split()            
+            f = open(os.environ['PUBLIC_JWT'], "r")
+            public_key = f.read()
+            jwt.unregister_algorithm('RS256')
+            jwt.register_algorithm('RS256', RSAAlgorithm(RSAAlgorithm.SHA256))
+            data = jwt.decode(token[1], public_key, verify_aud=False, algorithm='RS256')
+            valid = False                
+            for scope in data['scopes']:
+                if scope == "dados.tirar":
+                    valid = True
+            print("Scopes de token "+str(data['scopes']))
+            print("El scope buscado es dados.tirar, el token es "+str(valid))
+            if not valid:
                 return Response(status=status.HTTP_401_UNAUTHORIZED)
+            
         dados_array=[]
         for i in range(quantity):
             dados_array.append(random.randrange(1,7))
